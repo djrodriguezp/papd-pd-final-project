7a92cf70e5232e5403da991619d523f5d81ebd8a
Daniel Rodriguez <drodriguez@sonoc.io>
2021-12-13 02:01:28 -0600

    Added maps and change streamlit layout

--- Dockerfiles/streamlit/src/main.py
+++ Dockerfiles/streamlit/src/main.py
@@ -1,8 +1,11 @@
+import pydeck as pdk
+import numpy as np
 from sqlalchemy import create_engine
 import streamlit as st
 import pandas as pd
 from datetime import datetime
 
+st.set_page_config(layout="wide")
 
 @st.cache(allow_output_mutation=True)
 def load_covid_dataset():
@@ -10,6 +13,7 @@ def load_covid_dataset():
     db_conn = create_engine(db_source)
     df = pd.read_sql("SELECT * FROM global_data", con=db_conn)
     df["date"] = pd.to_datetime(df['date'], format='%Y-%m-%d').dt.date
+    df["state"] = df["state"].fillna('n/a')
     df = df.sort_values(['date'])
     return df
 
@@ -24,6 +28,15 @@ def filter_df(df, start_date, end_date, countries, states):
 
     return df[mask]
 
+
+def get_delta_count(df, status):
+    delta_df = df[(df['status'] == status)].groupby(['country', 'state', 'lat', 'lon'], dropna=True)['count'].agg(['first', 'last']).reset_index()
+    # diff = test['count']['last']-test['count']['first']
+    # diff
+    delta_df['count'] = delta_df['last'] - delta_df['first']
+    delta_df["count_str"] = delta_df["count"].map('{:,d}'.format)
+    delta_df.drop(columns=['first', 'last'], inplace=True)
+    return delta_df
 
 covid_df = load_covid_dataset()
 min_date = covid_df['date'].min()
@@ -57,9 +70,95 @@ states_filter = st.sidebar.multiselect(
     tuple(states_list)
 )
 
+status_filter = st.sidebar.selectbox(
+    "Tipo de Casos",
+    ('Confirmados', 'Recuperados', 'Muertes')
+)
+
+#filter_expander = st.expander("holi")
+#filter_expander.write("Holi")
 #st.dataframe(covid_df)
 #st.write(f"Usted ha elegido la fecha {end_date_filter} ")
 filtered_data = filter_df(covid_df, start_date_filter, end_date_filter, countries_filter, states_filter)
 
-filtered_data_map = filtered_data.dropna(subset=['lat','lon'])
-st.map(filtered_data_map)
+confirmed_cases_df = get_delta_count(filtered_data, "confirmed")
+recovered_cases_df = get_delta_count(filtered_data, "recovered")
+death_cases_df = get_delta_count(filtered_data, "deaths")
+
+map_df = None
+map_scatter_color = None
+if status_filter == "Confirmados":
+    map_df = confirmed_cases_df
+    map_scatter_color = [255, 0, 0, 128]
+    map_label = "Casos confirmados"
+elif status_filter == "Recuperados":
+    map_df = recovered_cases_df
+    map_scatter_color = [0, 255, 0, 128]
+    map_label = "Casos recuperados"
+else:
+    map_df = death_cases_df
+    map_scatter_color = [255, 51, 255, 128]
+    map_label = "Muertes"
+
+map_df = pd.DataFrame(map_df.dropna(subset=['lat','lon']))
+
+map_col1, map_col2 = st.columns([3, 1])
+
+map_col1.pydeck_chart(pdk.Deck(
+     map_style='dark',
+     initial_view_state=pdk.ViewState(
+         latitude=25,
+         longitude=0,
+         zoom=1
+     ),
+    layers=[
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=map_df,
+            radius_scale=1,
+            radius_min_pixels=3,
+            radius_max_pixels=25,
+            line_width_min_pixels=1,
+            get_position=['lon', 'lat'],
+            get_color=map_scatter_color,
+            get_radius="count",
+            opacity=0.7,
+            stroked=False,
+            filled=True,
+            pickable=True
+        ),
+    ],
+    tooltip={
+            "html": "<b>"+map_label+":</b> {count_str}"
+            "<br /><b>Country:</b> {country}"
+            "<br/> <b>Region/State:</b> {state}",
+            "style": {"color": "white"},
+        },
+ ))
+
+map_col2.metric(
+    "Casos Confirmados",
+    value = 100
+)
+
+map_col2.metric(
+    "Casos Confirmados",
+    value = 50
+)
+
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("Mi grafiquita 1")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("Mi grafiquita 2")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("Mi grafiquita 3")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
+
+#st.dataframe(filtered_data)
